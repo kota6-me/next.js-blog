30d94f4946da1033da2232b8d3cd847c05380202
kota6-me <contact@kota6.me>
2024-08-16 00:03:06 +0900

    修正

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -3,9 +3,9 @@ import Header from "../_components/header";
 
 export default function Home() {
   return (
-    <div className="absolute w-full h-fit top-0 left-0 selection:bg-orange-200">
+    <div className="absolute w-full h-fit top-0 left-0 text-black selection:bg-orange-200">
       <Header />
-      <div className="w-full lg:w-[64rem] lg:mx-auto p-5">TestBlog</div>
+      <div className="w-full lg:w-[64rem] lg:mx-auto p-5 pt-10">article</div>
     </div>
   );
 }
