f077184f795e78cc8442c091cddb79e912118dce
kota6-me <contact@kota6.me>
2024-08-17 13:02:59 +0900

    軽微な修正

--- app/(general)/page.tsx
+++ app/(general)/page.tsx
@@ -1,11 +1,14 @@
 "use client";
+import ArticleCard from "../_components/articleCard";
 import Header from "../_components/header";
 
 export default function Home() {
   return (
     <div className="absolute w-full h-fit top-0 left-0 text-black selection:bg-orange-200">
       <Header />
-      <div className="w-full lg:w-[64rem] lg:mx-auto p-5 pt-10">article</div>
+      <div className="relative w-full lg:w-[64rem] lg:mx-auto p-5 pt-32">
+        <ArticleCard />
+      </div>
     </div>
   );
 }
