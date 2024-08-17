d3a3344f8e3764035797f165897b4fc65c96c452
kota6-me <contact@kota6.me>
2024-08-17 13:02:21 +0900

    nextパッケージの読み出し修正

--- app/_components/navButton.tsx
+++ app/_components/navButton.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { usePathname as pathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 export default function NavBtn({
   href,
   icon,
@@ -11,7 +11,7 @@ export default function NavBtn({
 }) {
   return (
     <div className="col-span-1 group border-transparent hover:border-orange-400 border-2 border-solid duration-300 rounded-lg">
-      <Link href={pathname() === href ? "" : href}>
+      <Link href={usePathname() === href ? "" : href}>
         <div className="mx-auto h-fit w-fit">
           <span className="font-emj text-4xl text-black translate-y-0.5 duration-300 group-hover:-translate-y-0.5">
             {icon}
